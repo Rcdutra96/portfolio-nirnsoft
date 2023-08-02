d11df6f5ac2996690fae46c9b2874e7a20c2a905
Rcdutra96 <117887137+Rcdutra96@users.noreply.github.com>
2023-08-01 22:26:10 -0300

    Update About.jsx
    Preenchido com texto parcial do linkedin

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,19 +30,18 @@ const About = () => {
   return (
     <div className="-mt-[6rem]">
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
+        <p className={styles.sectionSubText}>Introdução</p>
+        <h2 className={styles.sectionHeadText}>Visão geral</h2>
       </motion.div>
 
       <motion.p
         variants={fadeIn('', '', 0.1, 1)}
         className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
-        sapiente ipsum dolorum dicta eaque cumque inventore molestias, beatae ea
-        quaerat alias accusamus voluptas autem! Alias odit voluptates in totam
-        vitae dignissimos minus eaque culpa unde tempore dolore aperiam
-        obcaecati voluptatum aliquam corrupti, suscipit accusamus! Odit unde
-        veniam dolorum ipsum doloribus.
+          Na NirnSoft, acreditamos que a combinação entre excelência técnica e foco no usuário é a chave para o sucesso. Utilizamos as mais recentes tecnologias e frameworks para criar aplicativos móveis e plataformas web que se destacam pela usabilidade, desempenho e design atraente.
+
+          Nossos serviços abrangem desde o desenvolvimento de aplicativos mobile nativos para iOS e Android, até a criação de soluções web escaláveis e seguras. Trabalhamos de forma ágil e colaborativa, garantindo entregas no prazo e resultados de alta qualidade.
+
+          Se você está em busca de uma equipe dedicada, experiente e comprometida em transformar suas ideias em realidade, a NirnSoft é a parceira ideal. Junte-se a nós e descubra como podemos impulsionar o crescimento e o sucesso do seu negócio por meio da tecnologia.
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
